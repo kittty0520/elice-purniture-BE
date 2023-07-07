4352586468a59d3f3dcceebb542b6dc9dbff44b8
[SW5] 최태관 <luvs2is@gmail.com>
2023-07-07 16:50:01 +0900

    feat. 모델 기능 추가

--- db/models/product-model.js
+++ db/models/product-model.js
@@ -1,55 +1,55 @@
-import { model } from "mongoose";
-import { ProductSchema } from "../schemas/product-schema";
+import { model } from 'mongoose';
+import { ProductSchema } from '../schemas/product-schema';
 
-const Product = model("products", ProductSchema);
+const Product = model('products', ProductSchema);
 
 export class ProductModel {
-  async findByTitle(title) {
-    const product = await Product.findOne({ title });
-    return product;
-  }
+    async findByTitle(title) {
+        const product = await Product.findOne({ title });
+        return product;
+    }
 
-  async findById(productId) {
-    const product = await Product.findOne({ _id: productId });
-    return product;
-  }
+    async findById(productId) {
+        const product = await Product.findOne({ _id: productId });
+        return product;
+    }
 
-  async findOneByCategoryId(categoryId) {
-    const product = await Product.findOne({ categoryId });
-    return product;
-  }
+    async findOneByCategoryId(categoryId) {
+        const product = await Product.findOne({ categoryId });
+        return product;
+    }
 
-  async findAllByCategoryId(categoryId) {
-    const products = await Product.find({ categoryId });
-    return products;
-  }
+    async findAllByCategoryId(categoryId) {
+        const products = await Product.find({ categoryId });
+        return products;
+    }
 
-  async create(productInfo) {
-    const createdNewProduct = await Product.create(productInfo);
-    return createdNewProduct;
-  }
+    async create(productInfo) {
+        const createdNewProduct = await Product.create(productInfo);
+        return createdNewProduct;
+    }
 
-  async findAll() {
-    const products = await Product.find({});
-    return products;
-  }
+    async findAll() {
+        const products = await Product.find({});
+        return products;
+    }
 
-  async update({ productId, update }) {
-    const filter = { _id: productId };
-    const option = { returnOriginal: false };
+    async update({ productId, update }) {
+        const filter = { _id: productId };
+        const option = { returnOriginal: false };
 
-    const updatedProduct = await Product.findOneAndUpdate(
-      filter,
-      update,
-      option
-    );
-    return updatedProduct;
-  }
+        const updatedProduct = await Product.findOneAndUpdate(
+            filter,
+            update,
+            option,
+        );
+        return updatedProduct;
+    }
 
-  async deleteById(productId) {
-    const result = await Product.deleteOne({ _id: productId });
-    return result;
-  }
+    async deleteById(productId) {
+        const result = await Product.deleteOne({ _id: productId });
+        return result;
+    }
 }
 
 const productModel = new ProductModel();
--- db/schemas/user-schema.js
+++ db/schemas/user-schema.js
@@ -1,50 +1,51 @@
-import {Schema} from "mongoose"
+import { Schema } from 'mongoose';
 
-const UserSchema = new Schema (
-  {
- userNumber: {
-    type:Number,
-    required: true,
-    unique: true,
-},
- email: {
-  type: String,
-  required: true,
-},
-password: {
-  type: String,
-  required: true,
-},
- fullName: {
-  type: String,
-  required: true,
-}, phoneNumber: {
-  type: String,
-  required: false,
-},
- address: {
-  type: new Schema(
+const UserSchema = new Schema(
     {
-      postalCode: String,
-      address1: String,
-      address2: String,
+        userNumber: {
+            type: Number,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: false,
+        },
+        address: {
+            type: new Schema(
+                {
+                    postalCode: String,
+                    address1: String,
+                    address2: String,
+                },
+                {
+                    _id: false,
+                },
+            ),
+            required: true,
+        },
+        role: {
+            type: String,
+            required: false,
+            default: 'basic-user',
+        },
     },
     {
-      _id: false,
-    }
-  ),
-  required: true,
-},
-role: {
-  type: String,
-  required: false,
-  default: "basic-user",
-},
-{
-  collection: "users",
-  timestamps: true,
-}
-  }
-)
+        collection: 'users',
+        timestamps: true,
+    },
+);
 
 export { UserSchema };
