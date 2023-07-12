ed5e62620c95692d392dd07959d4d65decda976a
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-07-12 17:08:28 +0900

    fix. path 수정

--- service/search_service.js
+++ service/search_service.js
@@ -1,19 +1,17 @@
-const categoryModel = require('../db/models/category_model').categoryModel;
-const productModel = require('../db/models/product_model').productModel;
+const searchModel = require('../db/models/search_model');
+const productModel = require('../db/models/product_model');
 
 // TODO: 실행되는지 확인 후 productService에 합치기
 class SearchService {
-    constructor(categoryModel, productModel) {
-        this.categoryModel = categoryModel;
-        this.productModel = productModel;
+    constructor(searchModel) {
+        this.searchModel = searchModel;
     }
     async getProductsByKeyword(keyword) {
-        const products = await this.productModel.findAllBykeyword(keyword);
-
+        const products = await this.searchModel.findAllBykeyword(keyword);
         return products;
     }
 }
 
-const searchService = new SearchService(productModel);
+const searchService = new SearchService(searchModel);
 
 module.exports = searchService;
