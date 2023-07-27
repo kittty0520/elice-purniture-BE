0db02f934b60bf46362130d98a26cac58502ff75
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-07-27 21:58:55 +0900

    fix. address 타입을 스키마 타입으로 변경 (원래대로)

--- db/schemas/types/address.js
+++ db/schemas/types/address.js
@@ -1,12 +1,16 @@
 const { Schema } = require('mongoose');
 
 const address = {
-    type: {
-        postalCode: String,
-        address1: String,
-        address2: String,
-    },
-
+    type: new Schema(
+        {
+            postalCode: String,
+            address1: String,
+            address2: String,
+        },
+        {
+            _id: false,
+        },
+    ),
     required: false,
 };
 module.exports = address;
