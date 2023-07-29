c6d5326a6749bbab8bc66e17100ae16d682908d4
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-07-29 18:03:37 +0900

    fix. options 객체 생성

--- db/models/order_model.js
+++ db/models/order_model.js
@@ -4,32 +4,30 @@ const OrderSchema = require('../schemas/order_schema');
 const Order = model('orders', OrderSchema);
 
 class OrderModel {
-    async findById(orderId) {
-        const order = await Order.findOne(
-            { _id: orderId },
-            { updatedAt: 0, __v: 0 },
-        ).populate('user', {
+    constructor() {
+        this.orderProjection = { updatedAt: 0, __v: 0 };
+        this.userPopulateOption = {
             password: 0,
             email: 0,
             role: 0,
             createdAt: 0,
             updatedAt: 0,
             __v: 0,
-        });
+        };
+    }
+    async findById(orderId) {
+        const order = await Order.findOne(
+            { _id: orderId },
+            this.orderProjection,
+        ).populate('user', this.userPopulateOption);
         return order;
     }
 
     async findAllByUserId(userId) {
         const orders = await Order.find(
             { user: userId },
-            { updatedAt: 0, __v: 0 },
-        ).populate('user', {
-            password: 0,
-            email: 0,
-            role: 0,
-            updatedAt: 0,
-            __v: 0,
-        });
+            this.orderProjection,
+        ).populate('user', this.userPopulateOption);
         return orders;
     }
 
@@ -39,16 +37,9 @@ class OrderModel {
     }
 
     async findAll() {
-        const orders = await Order.find({}, { updatedAt: 0, __v: 0 }).populate(
+        const orders = await Order.find({}, this.orderProjection).populate(
             'user',
-            {
-                password: 0,
-                email: 0,
-                role: 0,
-                createdAt: 0,
-                updatedAt: 0,
-                __v: 0,
-            },
+            this.userPopulateOption,
         );
         return orders;
     }
