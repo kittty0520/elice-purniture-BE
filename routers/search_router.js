ffca5de84d59d7de5755be3eeb99a69217a28e93
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-10 23:47:08 +0900

    remove. 필요없는 파일 삭제

--- routers/search_router.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const searchService = require('../service/search_service');
-const searchRouter = Router();
-
-// TODO: 실행되는지 확인 후 productRouter에 합치기
-
-
-module.exports = searchRouter;
--- service/duplicate_service.js
+++
@@ -1,8 +0,0 @@
-const userModel = require('../db/models/user_model');
-
-class DuplicateService {
-  
-}
-
-const duplicateService = new DuplicateService();
-module.exports = duplicateService;
